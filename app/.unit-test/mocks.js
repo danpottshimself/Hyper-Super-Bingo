5b779cbf31b79467f5d38c91bb714ba5fe26636e
Daniel Potts <Daniel.Potts@tombola.com>
2015-11-12 09:27:38 +0000

    Added Bower components, node modules, images, less and css. Also added unit tests.

---
+++ app/.unit-test/mocks.js
@@ -0,0 +1,31 @@
+var mocks = {
+
+    authenticateUser: {
+        logOutInformation : function(){},
+        nextGameInformation : function (){},
+        buyTicketInformation: function(){}
+    },
+    gameApi: {
+
+    },
+    userLogIn: {
+        handlePromise: function (tokenNumber) {
+        }
+    },
+    callingMethod: {
+    bingoCall: function (){}
+    },
+    gameTimer: {
+
+    },
+    ticketCreation:{
+
+    },
+    checkWinners: {
+
+    },
+    stateChange: {
+        go:function(gameState) {}
+    }
+
+};
