452745cb0a3650f4198a5f28805b1bb1ddad4bce
Daniel Potts <Daniel.Potts@tombola.com>
2015-11-23 17:05:27 +0000

    fixed the authentication for changing states before logging in.

--- app/scripts/run.js
+++ app/scripts/run.js
@@ -1,12 +1,11 @@
 (function () {
     'use strict';
     angular.module('Tombola.Module.Main')
-        .service('UserLogIn', ['$state', 'tokenService',
-            function ($state, tokenService) {
-            $rootScope.$on('$locationChangeSuccess', function() {
+        .run(['$rootScope', '$state', 'TokenService', function ($rootScope, $state, tokenService) {
+            $rootScope.$on('$locationChangeSuccess', function () {
                 if (!tokenService.isAuthenticated()) {
                     $state.go('logIn');
                 }
             });
-            }]);
+        }]);
 })();
